abacd0e2563e8f86a7bcc40e3a638236193e4f9e
hstoykova <hristina.pencheva@gmail.com>
2024-06-23 17:12:49 +0300

    Initial commit

---
+++ EntityFrameworkIntroduction/SoftUni/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace SoftUni
+{
+    internal class StartUp
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
