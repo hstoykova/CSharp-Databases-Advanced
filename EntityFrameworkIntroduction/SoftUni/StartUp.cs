dd1e1ab19180ceffd73290564b8438f0f95ecd62
hstoykova <hristina.pencheva@gmail.com>
2024-06-23 20:07:19 +0300

    "04. Employees with Salary Over 50 000" solved

--- EntityFrameworkIntroduction/SoftUni/StartUp.cs
+++ EntityFrameworkIntroduction/SoftUni/StartUp.cs
@@ -13,16 +13,20 @@ namespace SoftUni
             SoftUniContext context = new SoftUniContext();
             Console.WriteLine(GetEmployeesFullInformation(context));
 
-
+            //04.Employees with Salary Over 50 000
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
         }
 
+        //03
         public static string GetEmployeesFullInformation(SoftUniContext context)
         {
             // Option 1 for 03.Employees Full Information
+
             //return string.Join(Environment.NewLine, context.Employees
             //    .Select(e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}").ToList());
 
             // Option 2 for 03.Employees Full Information
+
             var employees = context.Employees
                 .Select(e => new
                 {
@@ -38,6 +42,29 @@ namespace SoftUni
             foreach (var e in employees)
             {
                 sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //04
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var richEmployees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var r in richEmployees)
+            {
+                sb.AppendLine($"{r.FirstName} - {r.Salary:F2}");
             }
 
             return sb.ToString().TrimEnd();
