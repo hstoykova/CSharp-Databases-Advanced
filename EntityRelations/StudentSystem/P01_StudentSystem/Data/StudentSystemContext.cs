4dcef2f7321728f023e71dc7944dabe46e21eec8
hstoykova <hristina.pencheva@gmail.com>
2024-06-28 13:29:46 +0300

    "01. Student System" solved

---
+++ EntityRelations/StudentSystem/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        //StudentSystem
+        private const string ConnectionString = "Server=HRISI\\SQLEXPRESS;Database=StudentSystem;Integrated Security=True;";
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentCourse> StudentsCourses { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionString);
+        //}
+
+        public StudentSystemContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        //Config composite primary key (Students_courses)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(c => new { c.StudentId, c.CourseId });
+
+            modelBuilder.Entity<Student>()
+                .Property(s => s.PhoneNumber)
+                .IsUnicode(false);
+        }
+    }
+}
