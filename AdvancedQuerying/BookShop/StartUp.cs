195408170f4ea8b1c79cb806104b395e7f3adb13
hstoykova <hristina.pencheva@gmail.com>
2024-07-04 13:48:50 +0300

    "02. Age Restriction" solved

---
+++ AdvancedQuerying/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByAgeRestriction(context, "teen"));
+        }
+
+        //02
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            if (!Enum.TryParse(command, true, out AgeRestriction ageRestriction)) 
+            {
+                return string.Empty;
+            }
+
+            var bookTitles = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => b.Title)
+                .OrderBy(t => t)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, bookTitles);
+        }
+    }
+}
+
+
