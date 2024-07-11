5a7f4ee9700802e35576cbee52ce3b78afa7c3f2
hstoykova <hristina.pencheva@gmail.com>
2024-07-11 17:16:53 +0300

    New folder created

---
+++ JavaScriptObjectNotationJSON/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+            //string userText = File.ReadAllText("../../../Datasets/users.json");
+            //Console.WriteLine(ImportUsers(context, userText));
+
+            string productText = File.ReadAllText("../../../Datasets/products.json");
+            Console.WriteLine(ImportUsers(context, productText));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count}";
+        }
+    }
+}
