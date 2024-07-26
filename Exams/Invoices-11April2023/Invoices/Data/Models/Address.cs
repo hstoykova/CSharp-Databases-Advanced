dc970e43031ee843d1c7f76731f58bfaf9732cfe
hstoykova <hristina.pencheva@gmail.com>
2024-07-26 11:54:19 +0300

    C# DB Advanced Retake Exam - 11 April 2023 - Model definition added

---
+++ Exams/Invoices-11April2023/Invoices/Data/Models/Address.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Invoices.Data.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string StreetName { get; set; } = null!;
+
+        [Required]
+        public int StreetNumber { get; set; }
+
+        [Required]
+        public string PostCode { get; set; } = null!;
+
+        [MaxLength(15)]
+        public string City { get; set; } = null!;
+
+        [MaxLength(15)]
+        public string Country { get; set; } = null!;
+
+        [ForeignKey(nameof(Client))]
+        public int ClientId { get; set; }
+        public virtual Client Client { get; set; } = null!;
+    }
+}
---
+++ Exams/Invoices-11April2023/Invoices/Data/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Invoices.Data.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(15)]
+        public string NumberVat { get; set; } = null!;
+
+        public virtual ICollection<Invoice> Invoices { get; set; } = new List<Invoice>();
+        public virtual ICollection<Address> Addresses { get; set; } = new List<Address>();
+        public virtual ICollection<ProductClient> ProductsClients { get; set; } = new List<ProductClient>();
+    }
+}
