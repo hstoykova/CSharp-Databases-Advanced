ec302bb39ae66ae3f5e48f47336ab462449e69fd
hstoykova <hristina.pencheva@gmail.com>
2024-07-28 12:45:08 +0300

    "C# DB Advanced Retake Exam - 11 December 2023" - model definition added

---
+++ Exams/Cadastre-11December2023/Cadastre/Data/Models/District.cs
@@ -0,0 +1,24 @@
+﻿using Cadastre.Data.Enumerations;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cadastre.Data.Models
+{
+    public class District
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(8)]
+        public string PostalCode { get; set; }
+
+        [Required]
+        public virtual Region Region { get; set; }
+
+        public virtual ICollection<Property> Properties { get; set; } = new List<Property>();
+    }
+}
